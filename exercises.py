54cae0adc24039db1a31607abfd61ae17e40dc7e
kikkedril <mvanderuitenbeek@letsconstruct.nl>
2018-06-24 19:57:43 +0200

    Added exercises

--- exercises.py
+++ exercises.py
@@ -1,56 +1,56 @@
-# a = 5
-# print(a)
-# a += 3  # this is the equivalent for a = a + 3
-# print(a)
-# b = 5
-# print(a + b)
-#
-# a = int(input("How much?"))
-# if (a >= 2):
-#     print("wow that's nice!")
-# elif (a < 2 and a >= 0):
-#     print("better improve on it")
-# else:
-#     print("oh whatever")
-#
-# a = 50
-# while a > 10:
-#     a -= 1  # this is equivalent to: a = a-1
-#     print(a)
-# print("end result: " + str(a))
-#
-# a = int(input("How much?"))
-# while a > 10:
-#     print("That is invalid. Try again")
-#     a = int(input("How much?"))
-# print("Good job! End result: " + str(a))
-#
-# import math
-#
-#
-# def calculate_distance(x1, y1, x2, y2):
-#     """This method calculates the distance between two points"""
-#     dx = x2 - x1
-#     dy = y2 - y1
-#     result = math.sqrt(dx ** 2 + dy ** 2)  # dx ** 2 is dx to the power of 2
-#     return result
-#
-#
-# # now reuse the method multiple times
-# print(calculate_distance(0, 0, 3, 4))  # result is 5, the famous 3, 4, 5 triangle
-# print(calculate_distance(0, 0, 6, 8))  # result is 10, the famous 6, 8, 10 triangle
-# print(calculate_distance(1, 10, 6, 22))  # result is 13, the famous 5, 12, 13 triangle
-#
-# divide_by = int(input("Divide by?"))
-# result = 0
-# try:
-#     result = 15 / divide_by
-# except Exception as e:
-#     print("Something is wrong with your calculation. Reverting to the default result: ")
-#     result = 15 / 2
-# finally:
-#     print(result)
-#
+a = 5
+print(a)
+a += 3  # this is the equivalent for a = a + 3
+print(a)
+b = 5
+print(a + b)
+
+a = int(input("How much?"))
+if (a >= 2):
+    print("wow that's nice!")
+elif (a < 2 and a >= 0):
+    print("better improve on it")
+else:
+    print("oh whatever")
+
+a = 50
+while a > 10:
+    a -= 1  # this is equivalent to: a = a-1
+    print(a)
+print("end result: " + str(a))
+
+a = int(input("How much?"))
+while a > 10:
+    print("That is invalid. Try again")
+    a = int(input("How much?"))
+print("Good job! End result: " + str(a))
+
+import math
+
+
+def calculate_distance(x1, y1, x2, y2):
+    """This method calculates the distance between two points"""
+    dx = x2 - x1
+    dy = y2 - y1
+    result = math.sqrt(dx ** 2 + dy ** 2)  # dx ** 2 is dx to the power of 2
+    return result
+
+
+# now reuse the method multiple times
+print(calculate_distance(0, 0, 3, 4))  # result is 5, the famous 3, 4, 5 triangle
+print(calculate_distance(0, 0, 6, 8))  # result is 10, the famous 6, 8, 10 triangle
+print(calculate_distance(1, 10, 6, 22))  # result is 13, the famous 5, 12, 13 triangle
+
+divide_by = int(input("Divide by?"))
+result = 0
+try:
+    result = 15 / divide_by
+except Exception as e:
+    print("Something is wrong with your calculation. Reverting to the default result: ")
+    result = 15 / 2
+finally:
+    print(result)
+
 import random
 
 
@@ -85,9 +85,9 @@ car2 = Car()
 car1.print_properties()
 car2.print_properties()
 
-# numbers = []
-# for i in range(0, 100, 3):
-#     print(i)
+numbers = []
+for i in range(0, 100, 3):
+    print(i)
 
 fruits = ["pear", "banana", "cherry", "orange"]
 for x in fruits:
